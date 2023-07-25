6390ee43b7742b5c6be4e5025fe351dd8f9694e0
Alcatraz1337 <lym0104@126.com>
2023-07-25 14:54:39 +0800

    Changed arm handling to server
    Depcrecated the arm handling in the worker and moved it to the server.
    Add a new parameter to the job description to specify the arm to use.
    Fixed a bug in the worker that caused the worker to crash when they get the same picking job.

--- catkin_ws/src/multi_nav_server/scripts/worker.py
+++ catkin_ws/src/multi_nav_server/scripts/worker.py
@@ -18,7 +18,7 @@ class Worker:
         self._stations = Stations().get_stations()
         self.sub_goal_result = rospy.Subscriber(self._name + '/move_base/result', MoveBaseActionResult,
                                                 self.goal_result_callback)
-        self.sub_arm_status = rospy.Subscriber('/arm_status', ArmStatus, self.arm_status_callback)
+        # self.sub_arm_status = rospy.Subscriber('/arm_status', ArmStatus, self.arm_status_callback) # Deprecated
         self.pub_cmd_vel = rospy.Publisher(self._name + '/cmd_vel', Twist, queue_size=1)
         self.pub_cancel_goal = rospy.Publisher(self._name + '/move_base/cancel', GoalID, queue_size=1)
         self.pub_car_status = rospy.Publisher('/car_status', CarStatus, queue_size=1)
@@ -26,14 +26,14 @@ class Worker:
         self.move_base_client.wait_for_server()
         self.pub_arm_work = rospy.Publisher('/arm_work', ArmWork, queue_size=1)
 
-        self._is_ready = True
-        self._is_moving = False
-        self._arm_picking = False
-        self._arm_dropping = False
-        self._arm_working = False
+        self.is_ready = True
+        self.is_moving = False
+        self.arm_picking = False
+        self.arm_dropping = False
+        self.arm_working = False
         self._job = ""  # Current job
         self._arm_obj_color = ""  # Working object
-        self._curr_target = -1  # Current target index
+        self.curr_target = -1  # Current target index
         self._next_target = -1  # Next target index
 
     def shutdown(self):
@@ -52,17 +52,17 @@ class Worker:
         # Set the car's target object and mark both signs to true
         self._job = job
         self._arm_obj_color = color
-        self._arm_picking = True
-        self._arm_dropping = True
+        self.arm_picking = True
+        self.arm_dropping = True
 
     def set_moving_targets(self, curr_target, next_target):
         # type: (int, int) -> None
         # Check if the targets are valid
         if curr_target < 0 or curr_target >= len(self._stations):
             rospy.logwarn("Invalid current target index")
-            self._curr_target = -1
+            self.curr_target = -1
         else:
-            self._curr_target = curr_target
+            self.curr_target = curr_target
 
         if next_target < 0 or next_target >= len(self._stations):
             rospy.logwarn("Invalid next target index")
@@ -73,73 +73,77 @@ class Worker:
     def activate_car(self):
         # type: () -> None
         # Error handling
-        if self._curr_target < 0 or self._curr_target >= len(self._stations):
+        if self.curr_target < 0 or self.curr_target >= len(self._stations):
             rospy.logwarn("Invalid current target index")
             return
-        if self._is_moving:
+        if self.is_moving:
             rospy.logwarn("Car " + str(self._id) + " is already moving")
             return
         # Send goal
         goal = MoveBaseGoal()
         # All the situation is go to output first then input
         # if the next target is -1, then should go to input
-        goal = self._stations[self._curr_target]._output if self._next_target != -1 \
-                                                        else self._stations[self._curr_target]._input
+        goal = self._stations[self.curr_target]._output if self._next_target != -1 \
+                                                        else self._stations[self.curr_target]._input
         goal.target_pose.header.frame_id = "map"
         self.move_base_client.send_goal(goal)
-        rospy.loginfo("Activate: Sending goal " + str(self._curr_target) + " to car " + str(self._id))
+        rospy.loginfo("Activate: Sending goal " + str(self.curr_target) + " to car " + str(self._id))
         # Move next target to current target
-        self._curr_target = self._next_target
+        self.curr_target = self._next_target
         self._next_target = -1
-        self._is_ready = False
-        self._is_moving = True
+        self.is_ready = False
+        self.is_moving = True
 
     def arm_pick(self, obj=""):
         # type: (str) -> None
         # TODO: Set arm to pick specific object
         msg = ArmWork(self._id, "pick", self._job, obj)
         self.pub_arm_work.publish(msg)
-        self._arm_working = True
+        self.arm_working = True
 
     def arm_drop(self):
         # TODO: Set arm to drop the object
         msg = ArmWork(self._id, "drop", self._job, "")
         self.pub_arm_work.publish(msg)
-        self._arm_working = True
+        self.arm_working = True
 
     def goal_result_callback(self, msg):
         # type: (MoveBaseActionResult) -> None
         if msg.status.status == 3:
             rospy.loginfo("Goal reached, setting car " + str(self._id) + " not moving, continuing to arm...")
-            self._is_moving = False
-            if self._arm_picking: 
+            self.is_moving = False
+            if self.arm_picking: 
                 # If the car has an object to pick, then pick it
                 self.arm_pick(self._arm_obj_color)
             else:
                 # If the car has no object to pick, then drop the object
                 self.arm_drop()
 
+    """
+    The following code is deprecated due to all arm related functions are handled in the server
     def arm_status_callback(self, msg):
         # type: (ArmStatus) -> None
         # TODO: When arm finished moving, update arm status
         if msg.arm_id == self._id and msg.status:
             rospy.loginfo("Car " + str(self._id) + " arm finished!")
-            if self._is_moving:
+            if self.is_moving:
                 # If the car is still moving, the arm should not be working..
                 # TODO: Fix the problem if the arm is working when the car is moving
                 rospy.logwarn("Car " + str(self._id) + " is moving, but arm is working, this should not happen!")
                 return
             # The first call should be the arm finished picking, so the next move is to go the next target
-            if self._arm_picking:
+            if self.arm_picking:
                 rospy.loginfo(
-                    "Car " + str(self._id) + " finished picking, sending goal " + str(self._curr_target) + "...")
-                self._arm_picking = False
+                    "Car " + str(self._id) + " finished picking, sending goal " + str(self.curr_target) + "...")
+                self.arm_picking = False
                 self.activate_car()
             # The second call should be the arm finished dropping, so the next move is set the car to ready
-            elif self._arm_dropping:
+            elif self.arm_dropping:
                 rospy.loginfo("Car " + str(self._id) + " finished dropping, setting car to ready...")
-                self._arm_dropping = False
-                self._arm_working = False
-                self._is_ready = True
+                self.arm_dropping = False
+                self.arm_working = False
+                self.is_ready = True
                 # Publish car status
-                self.pub_car_status.publish(CarStatus(self._id, self._job, self._is_ready))
+                self.pub_car_status.publish(CarStatus(self._id, self._job, self.is_ready))
+
+    """
